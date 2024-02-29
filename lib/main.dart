7a12327a8a9762982055c938cd8f680fbbf1e599
CodingCloud79 <160732070+CodingCloud79@users.noreply.github.com>
2024-02-29 18:46:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_background/flutter_background.dart';
+
+import 'call_state.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeService();
+  runApp(const MyApp());
+}
+
+String appState = 'nothing';
+
+Future<void> initializeService() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  print(appState);
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initBackgroundExecution();
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CallStateApp(),
+    );
+  }
+
+  Future<void> initBackgroundExecution() async {
+    final androidConfig =  FlutterBackgroundAndroidConfig(
+      notificationTitle: "flutter_background example app",
+      notificationText:
+          "Background notification for keeping the example app running in the background",
+      notificationImportance: AndroidNotificationImportance.Default,
+      notificationIcon: AndroidResource(
+          name: 'background_icon',
+          defType: 'drawable'), // Default is ic_launcher from folder mipmap
+    );
+    bool success =
+        await FlutterBackground.initialize(androidConfig: androidConfig);
+
+    debugPrint("$success");
+  }
+}
